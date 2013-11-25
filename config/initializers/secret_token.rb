967a12704ce2cac4d69fd6b833a3c7fe6210b6ec
jeesong <jeewsong@gmail.com>
2013-11-25 14:56:50 -0800

    App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaytrafficApp::Application.config.secret_token = '80779f51658203d7f80569b0afcb9a5ad3da0639cd33f7bd12f2db17dc888d3d9ad68e721cb2e381de7d7089f68eca6f4bf2939d3263e2ca743d3a19dcf769fe'
